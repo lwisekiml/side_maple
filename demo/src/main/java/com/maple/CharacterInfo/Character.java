09b2836fdf694b58a19ae99b66050be7cc85a17b
wisekim <lwisekiml@naver.com>
2024-01-11 22:11:06 +0900

    캐릭터 기본 정보 조회 JSON 파싱 테스트

---
+++ demo/src/main/java/com/maple/CharacterInfo/Character.java
@@ -0,0 +1,20 @@
+package com.maple.CharacterInfo;
+
+import lombok.Data;
+
+@Data
+public class Character {
+
+    String date;
+    String character_name;
+    String world_name;
+    String character_gender;
+    String character_class;
+    String character_class_level;
+    long character_level;
+    long character_exp;
+    String character_exp_rate;
+    String character_guild_name;
+    String character_image;
+
+}
--- demo/src/main/java/com/maple/test/ApiTest.java
+++ demo/src/main/java/com/maple/test/ApiTest.java
@@ -1,5 +1,9 @@
 package com.maple.test;
 
+import com.maple.CharacterInfo.Character;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -42,7 +46,25 @@ public class ApiTest {
             }
             in.close();
 
-            System.out.println("결과 : " + response.toString());
+            Character character = new Character();
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(response.toString());
+
+            character.setDate((String) jsonObject.get("date"));
+            character.setCharacter_name((String) jsonObject.get("character_name"));
+            character.setWorld_name((String) jsonObject.get("world_name"));
+            character.setCharacter_gender((String) jsonObject.get("character_gender"));
+            character.setCharacter_class((String) jsonObject.get("character_class")) ;
+            character.setCharacter_class_level((String) jsonObject.get("character_class_level"));
+            character.setCharacter_level((long) jsonObject.get("character_level"));
+            character.setCharacter_exp((long) jsonObject.get("character_exp"));
+            character.setCharacter_exp_rate((String) jsonObject.get("character_exp_rate"));
+            character.setCharacter_guild_name((String) jsonObject.get("character_guild_name"));
+            character.setCharacter_image((String) jsonObject.get("character_image"));
+
+            System.out.println("character.toString() : " + character.toString());
+
         } catch (Exception exception) {
             System.out.println(exception);
         }
