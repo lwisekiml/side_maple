c072ed98a379a05ecd0243d4263aa78754a29fcf
wisekim <lwisekiml@naver.com>
2024-01-16 22:35:35 +0900

    캐릭터 기본 정보 조회를 controller, service 사용하여 수정

--- demo/src/main/java/com/maple/controller/character/CharacterController.java
+++ demo/src/main/java/com/maple/controller/character/CharacterController.java
@@ -1,11 +1,18 @@
 package com.maple.controller.character;
 
-import com.maple.domain.Character;
 import com.maple.service.character.CharacterService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping("/character")
@@ -14,8 +21,42 @@ public class CharacterController {
 
     private final CharacterService characterService;
 
+    @SneakyThrows
     @GetMapping("/basic") // 기본 정보 조회
-    public void basic(Character character) {
-       characterService.save(character);
+    public void basic() {
+        //////////////////////////////////////////////////////////////////////////////
+        String API_KEY = "";
+        String characterName = URLEncoder.encode("", StandardCharsets.UTF_8);
+        String ocid = "";
+        String date = "2023-12-21";
+
+        String urlString = "";
+        URL url = new URL(urlString);
+        //////////////////////////////////////////////////////////////////////////////
+
+        // HTTP connection 설정
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("x-nxopen-api-key", API_KEY);
+
+        int responseCode = connection.getResponseCode();
+
+        BufferedReader in;
+        if(responseCode == 200) {
+            // responseCode 200 정상응답
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        } else {
+            // responseCode 200 이외의 코드가 반환되었을 경우
+            in = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        }
+
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+       characterService.save(response);
     }
 }
--- demo/src/main/java/com/maple/service/character/CharacterService.java
+++ demo/src/main/java/com/maple/service/character/CharacterService.java
@@ -3,6 +3,9 @@ package com.maple.service.character;
 import com.maple.domain.Character;
 import com.maple.repository.character.CharacterRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +14,29 @@ public class CharacterService {
 
     private final CharacterRepository characterRepository;
 
-    public void save(Character character) {
-        characterRepository.save(character);
+    @SneakyThrows
+    public void save(StringBuffer response) {
+
+        // 캐릭터 기본 정보 조회
+        Character character = new Character();
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(response.toString());
+
+        character.setDate((String) jsonObject.get("date"));
+        character.setCharacter_name((String) jsonObject.get("character_name"));
+        character.setWorld_name((String) jsonObject.get("world_name"));
+        character.setCharacter_gender((String) jsonObject.get("character_gender"));
+        character.setCharacter_class((String) jsonObject.get("character_class")) ;
+        character.setCharacter_class_level((String) jsonObject.get("character_class_level"));
+        character.setCharacter_level((long) jsonObject.get("character_level"));
+        character.setCharacter_exp((long) jsonObject.get("character_exp"));
+        character.setCharacter_exp_rate((String) jsonObject.get("character_exp_rate"));
+        character.setCharacter_guild_name((String) jsonObject.get("character_guild_name"));
+        character.setCharacter_image((String) jsonObject.get("character_image"));
+
+        System.out.println("character.toString() : " + character.toString());
+
+//        characterRepository.save(character);
     }
 }
