df1778a64c0287eea63202fb45d7d36f509f5ad8
wisekim <lwisekiml@naver.com>
2024-01-23 13:38:23 +0900

    스프링으로 전환 테스트

--- demo/src/main/java/com/maple/test/AppConfig.java
+++ demo/src/main/java/com/maple/test/AppConfig.java
@@ -4,13 +4,19 @@ import com.maple.test.member.MemberRepository;
 import com.maple.test.member.MemberService;
 import com.maple.test.member.MemberServiceImpl;
 import com.maple.test.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// 스프링으로 전환
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
--- demo/src/main/java/com/maple/test/MemberApp.java
+++ demo/src/main/java/com/maple/test/MemberApp.java
@@ -3,12 +3,18 @@ package com.maple.test;
 import com.maple.test.member.Grade;
 import com.maple.test.member.Member;
 import com.maple.test.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // member 실행 및 테스트
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
