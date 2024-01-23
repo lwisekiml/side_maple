38b105cb113f6b86c079ae6503f5480d0d1b7a4a
wisekim <lwisekiml@naver.com>
2024-01-23 13:39:02 +0900

    ApiTest import 추가

--- demo/src/main/java/com/maple/test/ApiTest.java
+++ demo/src/main/java/com/maple/test/ApiTest.java
@@ -10,6 +10,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
