b43ca97c08521abafeb23642cf3186e7adee008f
wisekim <lwisekiml@naver.com>
2024-01-26 22:30:00 +0900

    Character 생성

--- demo/src/main/java/com/maple/domain/Character.java
+++ demo/src/main/java/com/maple/domain/Character.java
@@ -1,21 +1,5 @@
 package com.maple.domain;
 
-import lombok.Data;
-
-@Data
 public class Character {
-
-    Long id;
-    String date;
-    String character_name;
-    String world_name;
-    String character_gender;
-    String character_class;
-    String character_class_level;
-    long character_level;
-    long character_exp;
-    String character_exp_rate;
-    String character_guild_name;
-    String character_image;
-
+    String ocid;
 }
