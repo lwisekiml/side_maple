cbd7291c3019a626d6da7d0dfe57583d33d0e60a
wisekim <lwisekiml@naver.com>
2024-01-27 20:10:58 +0900

    sql문 추가

--- demo/sql/ddl.sql
+++ demo/sql/ddl.sql
@@ -1,4 +1,6 @@
-create table chracter_basic
+use maple;
+
+create table character_basic
 (
     id bigint auto_increment,
     ocid varchar(35),
@@ -15,3 +17,49 @@ create table chracter_basic
     character_image varchar(400),
     primary key (id)
 );
+
+alter table character_basic drop primary key, add primary key(id, ocid);
+
+-- 테스트
+insert into character_basic (
+    ocid,
+    date,
+    character_name,
+    world_name,
+    character_gender,
+    character_class,
+    character_class_level,
+    character_level,
+    character_exp,
+    character_exp_rate,
+    character_guild_name,
+    character_image
+) values (
+             'ch_ocid',
+             'test',
+             'chracter_basic',
+             'test',
+             'M',
+             'test',
+             '5',
+             1,
+             2,
+             'chract',
+             'test',
+             'chracter_basic'
+         )
+
+----------------------------------------------------------
+
+create table character_popularity
+(
+    ocid varchar(35),
+    date varchar(25),
+    popularity BIGINT,
+    primary key (ocid)
+);
+
+alter table character_popularity add primary key(ocid);
+
+----------------------------------------------------------
+
